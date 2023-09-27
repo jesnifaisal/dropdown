c2e37f205d9cecd68b8b92d9ef62a5c7389af1e9
jesni faisal kf <141605834+jesnifaisal@users.noreply.github.com>
2023-09-27 11:41:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Drop down",
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const dropdown(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class dropdown extends StatefulWidget {
+  const dropdown({Key? key}) : super(key: key);
+
+  @override
+  _dropdownState createState() => _dropdownState();
+}
+
+class _dropdownState extends State<dropdown> {
+  String dropdownvalue = 'Item 1';
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      appBar: AppBar(
+        title: const Text("DROP DOWN "),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton(
+              value: dropdownvalue,
+              icon: const Icon(Icons.keyboard_arrow_down),
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
